896e77ee948af5103f52724bf45bf6ef0fcb3e2b
Daniel Glaser <glaserd92@gmail.com>
2016-08-04 11:46:23 -0500

    (style) update to functional component

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,10 +1,10 @@
+/* eslint-env browser */
+
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 
-class App extends React.Component {
-  render () {
-    return <p>Hello!!!!</p>;
-  }
-}
+const App = () => (
+  <p>Hello!!!!</p>
+);
 
-render(<App/>, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
