4319e8d2cee79e0a42aa031f3bbfd53d06072950
Daniel Glaser <glaserd92@gmail.com>
2016-08-04 16:48:30 -0500

    (chores) Restructure architecture

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,24 @@ var webpack = require('webpack');
 var path = require('path');
 
 
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'client/app');
+var BUILD_DIR = path.resolve(__dirname, 'client/public');
 
 var config = {
-  entry: APP_DIR + "/index.jsx",
+  entry: APP_DIR + '/index.jsx',
   output: {
     path: BUILD_DIR,
-    filename: "bundle.js"
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       {
         test: /\.jsx?/,
         include: APP_DIR,
-        loader: 'babel'
-      }
-    ]
-  }
+        loader: 'babel',
+      },
+    ],
+  },
 };
 
 module.exports = config;
