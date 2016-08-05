025262bd4e50927fc6bb8c8c62f454f4374a0ef7
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-04 22:45:49 -0500

    (chore) add dotenv update npm dev script update npm script for production [closes #26]

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import server from './server';
+
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config();
+}
 
 const app = express();
 
