44b458b67269b5622a1f8c33de7b3324b2baf610
Daniel Glaser <glaserd92@gmail.com>
2016-08-08 11:07:55 -0500

    (feat) Add base template for search and button components

--- client/app/components/App.jsx
+++ client/app/components/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Button from './Button.jsx';
+import Search from './Search.jsx';
 
 const App = () => (
-  <p>Hello!!!!</p>
+  <div className="container">
+    <h1>FoodCompass</h1>
+    <p>What do you want to eat?</p>
+    <Search />
+    <Button />
+  </div>
 );
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//       <p>Hello!!!!</p>
+//     );
+//   }
+// }
 
 export default App;
