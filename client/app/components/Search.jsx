ab8aeb4e9526299101675306d42eed161755605e
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-09 13:53:50 -0500

    (fix) change incorrect autoFocus attribute

--- client/app/components/Search.jsx
+++ client/app/components/Search.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Search = () => (
   <div>
-    <input type="text" name="foodsearch" autofocus/>
+    <input type="text" name="foodsearch" autoFocus />
   </div>
 );
 
