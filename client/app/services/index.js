f86d87199d2f00d9260d466a2d35a9eab5e7b052
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-09 18:55:47 -0500

    (style) change let to const per eslint

--- client/app/services/index.js
+++ client/app/services/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const services = {
   searchYelp: (food, lat, long) => {
-    let reqURI = `/api/v1/search?food=${food}&cll=${lat},${long}`; 
+    const reqURI = `/api/v1/search?food=${food}&cll=${lat},${long}`;
     axios.get(reqURI)
       .then(res => {
         console.log('yelp search response:', res);
