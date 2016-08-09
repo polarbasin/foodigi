d00d187e4d69cc780334bebfc28dd99552777706
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-08 21:13:10 -0500

    (feat) add continuous geoLocation watch on componentDidMount add error messages and update count to Coords component

--- client/app/helpers/index.js
+++ client/app/helpers/index.js
@@ -7,14 +7,38 @@ const helpers = {
       return {};
     }
     return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(position => {
-        if (!position) {
-          reject(position);
+      navigator.geolocation.getCurrentPosition((position, err) => {
+        if (err) {
+          reject(err);
         } else {
           resolve(position.coords);
         }
       });
     });
+  },
+
+  watchGeolocation: (callback) => {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/watchPosition
+    if (!('geolocation' in navigator)) {
+      callback({
+        message: 'geolocation required.',
+        err: 'Geolocation not supported',
+      });
+    } else {
+      const id = navigator.geolocation.watchPosition((position) => {
+        // need to cancel id at some point
+        callback(null, position, id);
+      }, (err) => {
+        callback({
+          message: 'Error getting location',
+          err,
+        });
+      }, {
+        enableHighAccuracy: false,
+        maximumAge: 0,
+        timeout: 500,
+      });
+    }
   },
 };
 
