e576400c46a6e6583bdff6c3d8e17f95a829cce6
Daniel Glaser <glaserd92@gmail.com>
2016-08-10 18:24:36 -0500

    (feat) Populate data from YELP API

--- client/app/services/index.js
+++ client/app/services/index.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 const services = {
   searchYelp: (food, lat, long) => {
     const reqURI = `/api/v1/search?food=${food}&cll=${lat},${long}`;
-    axios.get(reqURI)
-      .then(res => {
-        console.log('yelp search response:', res.data);
-      })
-      .catch(err => {
-        console.error('Error searching Yelp', err);
-      });
+    return axios.get(reqURI)
+      .then(results =>
+        // handle error
+        new Promise((resolve) => {
+          resolve(results.data);
+        })
+      );
   },
 };
 
