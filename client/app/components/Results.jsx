245a1f83a5f8af80c4593df171e744a1a53680e7
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-12 18:18:01 -0500

    (chore) Move custom console logs to bottom of page

--- client/app/components/Results.jsx
+++ client/app/components/Results.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 import FoodResults from './dev_components/FoodResults.jsx';
 import Compass from './Compass.jsx';
 
-const Results = ({ compassHeading, foodData, origin }) => (
+const Results = ({ compassHeading, foodData, origin, foodQuery }) => (
   <div id="results" className="search-results">
-    <FoodResults foodData={foodData} origin={origin} compassHeading={compassHeading} />
     <Compass
       compassHeading={compassHeading}
       origin={origin}
       dest={foodData.location.coordinate}
+      foodQuery={foodQuery}
     />
+    <FoodResults foodData={foodData} origin={origin} compassHeading={compassHeading} />
   </div>
 );
 
@@ -18,6 +19,7 @@ Results.propTypes = {
   compassHeading: React.PropTypes.number,
   foodData: React.PropTypes.object,
   origin: React.PropTypes.object,
+  foodQuery: React.PropTypes.string,
 };
 
 export default Results;
